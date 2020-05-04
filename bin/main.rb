20962be072b6671ba047fb0c08f24f83d3ff6bc2
ezeilo-su <ezeilo.su@gmail.com>
2020-05-04 18:46:11 +0100

    create game logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
-puts "Hello world!"
+load "../lib/game-logic.rb" 
+
+def print_user_message(msg)
+  print msg
+end
+
+board1 = Board.new
+board1.render_board
+
+# p board1.diagonals
+
+
