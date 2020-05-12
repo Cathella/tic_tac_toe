bfb0c2f1dafa09bd7081f2f9e35568f71825ff6c
ezeilo-su <ezeilo.su@gmail.com>
2020-05-12 16:15:04 +0100

    Update local files

--- bin/main.rb
+++ bin/main.rb
@@ -2,6 +2,7 @@
 require_relative "../lib/board.rb"
 require_relative "../lib/player.rb"
 require_relative "../lib/game_logic.rb"
+require 'colorize'
 
 $game = TicTacToeGame.new
 # $game.board = Board.new
@@ -29,7 +30,7 @@ def get_player_name(player_name)
   loop do
     print "#{player_name} enter your name as a text without a space or symbol: "
     name = gets.chomp
-    if !(name =~ /^[0-9a-zA-Z]+$/).nil?      
+    if !(name =~ /^[0-9a-zA-Z]{1,10}$/).nil?  #/^[0-9a-zA-Z]+$/    
       return name.capitalize # capitalize the first letter of the name
     else
       puts "Invalid name format, try again!"
@@ -41,7 +42,7 @@ def get_player_symbol(player_name)
   loop do
     print "#{player_name} enter your symbol as just one letter: "
     piece = gets.chomp
-    if (piece =~ /[a-zA-Z]/).nil? || ($game.player_a.piece == piece.upcase || $game.player_b.piece == piece.upcase)
+    if (piece =~ /^[a-zA-Z]$/).nil? || ($game.player_a.piece == piece.upcase || $game.player_b.piece == piece.upcase)
       puts "Invalid symbol format, or symbol already exists. Try again!"      
     else
       return @piece = piece.upcase
