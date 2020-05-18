b39a33a518398f92a2aad5e078b4c511e61bdcff
ezeilo-su <ezeilo.su@gmail.com>
2020-05-18 22:10:52 +0100

    Create test cases for the method, empty?

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,4 +1,6 @@
-require_relative '../bin/main.rb'
+require_relative '../lib/player.rb'
+require_relative '../lib/board.rb'
+require_relative '../lib/game_logic.rb'
 
  describe Board do
   let (:board) { Board.new }
@@ -6,8 +8,14 @@ require_relative '../bin/main.rb'
   O = 'O'
 
   describe '#empty?' do
-    it '' do
-      
+    it "it should return 'true' if a cell is not 'X' or 'O'" do
+      board.display = [1, X, 3, 4, 5, X, O, 8, 9]
+      expect(board.empty?(3)).to eq(true)
     end
+
+    it "it should return 'false' if a cell is 'X' or 'O'" do
+      board.display = [1, X, 3, 4, 5, X, O, 8, 9]
+      expect(board.empty?(7)).to eq(false)
+    end    
   end
  end
